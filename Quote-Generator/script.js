e563a4cae052b9d8578fe0e968ddcc1011c786b3
Deepak Shreevastava <Dpkshreevastava@gmail.com>
2022-12-29 01:42:20 +0530

    fetching w/o proxy

--- Quote-Generator/script.js
+++ Quote-Generator/script.js
@@ -11,7 +11,7 @@ async function getQuote() {
     const proxyURL = 'https://fullydeepak.github.io/vannila-js-projects/Quote-Generator/';
     const apiURL = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
     try {
-      const response = await fetch(proxyURL + apiURL);
+      const response = await fetch(apiURL);
       const data = await response.json();
       console.log(data);
       //If didn't fetch any author name
