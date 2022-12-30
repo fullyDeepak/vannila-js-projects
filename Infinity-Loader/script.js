388462afa418c66b42a86a4a995f0697fa476986
Deepak Shreevastava <Dpkshreevastava@gmail.com>
2022-12-30 12:51:30 +0530

    JavaScript Fix

--- Infinity-Loader/script.js
+++ Infinity-Loader/script.js
@@ -1,54 +1,53 @@
-const imageContainer = document.getElementById('image-container');
-const loader = document.getElementById('loader');
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
 let photosArr = [];
 
 //Check if all images were loaded
-function imageLoaded(){
-   imagesLoaded++;
-   if (imagesLoaded === totalImages){
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
     ready = true;
-    loader.hidden = true;   
-   }
+    loader.hidden = true;
+  }
 }
 
 //Helper function to Set Attributes on DOM elements
-function setAttribute(element, attributes){
-    for (const key in attributes){
-        element.setAttribute(key, attributes[key]);
-    }
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
 }
 
-
 //Create elements for links & photos and add to DOM
-function displayPhotos(){
-    imagesLoaded = 0;
-    totalImages = photosArr.length;
-    photosArr.forEach((photo) => {
-        //Creating <a> to link to Unsplash
-        const item = document.createElement('a');
-        setAttribute(item, {
-            href: photo.links.html,
-            targer: '_blank',
-        });
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArr.length;
+  photosArr.forEach((photo) => {
+    //Creating <a> to link to Unsplash
+    const item = document.createElement('a');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
 
-        //Create <img> for photos
-        const  img = document.createElement('img');
-        setAttribute(item,{
-            scr:photo.url.regular,
-            alt:photo.alt_description,
-            title: photo.alt_description,
-        });
+    //Create <img> for photos
+    const img = document.createElement('img');
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
 
-        //Event Listener, check when each is finished loading
-        img.addEventListener('load', imageLoaded);
+    //Event Listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded);
 
-        //Put <img> inside <a>, then put both inside imageContainer Element
-        item.appendChild(img);
-        imageContainer.appendChild(item);
-    })
+    //Put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
 }
 
 const count = 10;
@@ -61,18 +60,18 @@ async function getPhotos() {
     const response = await fetch(apiURL);
     photosArr = await response.json();
     displayPhotos();
-  } catch (error) {
-
-  }
+  } catch (error) {}
 }
 
-
 //Check to see if scrolling near bottom of page, Load more Photos
-window.addEventListener('scroll', () =>{
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
-        ready = false;
-        getPhotos();
-    }
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
 });
 
 //On load
